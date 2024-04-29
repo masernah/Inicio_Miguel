c916699c806238a1732297ff69da294734ba5551
Leopoldo <Leopoldo@Zandra>
2024-04-29 11:49:12 +0200

    hola miguel ya verifique el codigo y lo corregi

--- src/Hola.java
+++ src/Hola.java
@@ -3,6 +3,8 @@ public class Hola {
 	
 	public static void main (String[]arg){
 		System.out.println("Hola Soy Miguel Serna");
+		System.out.println("Hola miguel");
+		System.out.println("Soy Leopoldo"); 		
 	}
 
 }
